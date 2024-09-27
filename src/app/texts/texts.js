849e950efa8bf0fd7162c4154f14c8dd9c29f555
kevinpicon01 <digitalpruu@gmail.com>
2024-09-27 16:51:08 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,10 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "RIVEPAN";
+const email = "rivecastell@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    "¡Hola! Estoy interesado en conocer más sobre los productos de " + title + " y me gustaría obtener más información. ¡Gracias!"
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "+573212474344"; // Incluido el prefijo +57
+
 const textos = {
     index: {
         title: title,
@@ -12,25 +13,25 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "En RIVEPAN, nos dedicamos a la elaboración de productos de panadería artesanal como pan de verduras, tortas, galletas y empanadas, ofreciendo opciones sabrosas y saludables para todos nuestros clientes.",
+        description2: "Nuestra misión es llevar a tu mesa productos frescos y de calidad, hechos con dedicación y pasión. Nos enfocamos en combinar ingredientes naturales y recetas tradicionales para brindarte un sabor único y nutritivo. Ya sea que estés buscando algo delicioso para una ocasión especial o simplemente quieras disfrutar de un bocadillo saludable, RIVEPAN tiene lo que necesitas. ¡Déjanos ser parte de tus momentos más deliciosos!"
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Rico pan, mejor sabor.",
+            p2: ""
         },
-        address: "Dirección del emprendimiento",
+        address: "", // Información no proporcionada
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/riveiro.castellanos?mibextid=ZbWKwL", // Enlace a Facebook proporcionado
+        instagram: "https://www.instagram.com/", // No se proporcionó enlace de Instagram
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
     },
-
 };
+
 export default textos;
